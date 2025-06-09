561a9d6f51a95eaf901f2e66489f6399125a9174
lavanya112317 <patlollalavanya345@gmail.com>
2025-06-09 12:00:08 +0530

    Create Print2Darray.java

---
+++ Print2Darray.java
@@ -0,0 +1,24 @@
+//print 2D array
+import java.util.scanner;
+public class Print2Darray{
+  public static void main(string[]args){
+  Scanner sc=new Scanner(System.in);
+  int rows=sc.nextInt();
+  System.out.println("enter the number of rows");
+  int cols=sc.nextInt();
+  System.out.println("enter the number of columns");
+  int[][]arr= new int[rows][cols];
+for(i=0;i<rows;i++){
+  for(j=0;j<cols;j++){
+    arr[i][j]=sc.nextInt();
+  }
+}
+for(i=0;i<rows;i++){
+  for(j=0;j<cols;j++){
+    System.out.println(arr[i][j]);
+  }
+}
+}
+}
+  
+A
