b853dbc3fc189b97f94fa0bb796b5450b8114020
lavanya112317 <patlollalavanya345@gmail.com>
2025-06-09 12:13:27 +0530

    Create Wrongoutput.java

---
+++ Wrongoutput.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+public class wrongoutput{
+  public static void main(string[]args){
+    Scanner sc=new Scanner(System.in);
+    int[][]arr=new int[2][3];
+    for(i=0;i<2;i++){
+      for(j=0;j<3;j++){
+        a[i][j]=sc.nextInt();
+      }
+    }
+    for(i=0;i<3;i++){
+      for(j=0;j<2;j++){
+        System.out.print(a[i][j]+" ");
+      }
+       System.out.println();
+    }
+  }
+}  
